d07d5eacdd0c9539a917376c41a6d807ca940d45
serega <serstalk@gmail.com>
2020-05-27 20:44:41 +0300

    SoketChat, first

---
+++ src/main/java/Client.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
+public class Client {
+    private static final int PORT = 19000;
+    private static final String HOST = "localhost";
+
+    public static void main(String[] args) throws IOException {
+
+        Socket clientSocket = null;
+        try{
+            clientSocket = new Socket(HOST, PORT);
+
+            try(InputStream in = clientSocket.getInputStream();
+                OutputStream out = clientSocket.getOutputStream()){
+
+                String line = "Hello";
+                out.write(line.getBytes());
+                out.flush();
+
+                byte[] data = new byte[32 * 1024];
+                int readBytes = in.read(data);
+
+                System.out.printf("Server> %s", new String(data, 0, readBytes));
+
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static final int PORT = 19000;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try{
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Started, waiting for connection");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Accepted. " + clientSocket.getInetAddress());
+
+            try(InputStream in = clientSocket.getInputStream();
+                OutputStream out = clientSocket.getOutputStream()){
+
+                byte[] buf = new byte[32 * 1024];
+                int readBytes = in.read(buf);
+                String line = new String(buf, 0, readBytes);
+                System.out.printf("Client>%s", line);
+
+                out.write(line.getBytes());
+                out.flush();
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            serverSocket.close();
+        }
+
+
+
+    }
+
+    public static int printOlolo(){
+        return 10;
+    }
+
+}
