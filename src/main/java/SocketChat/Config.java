6aaefa1f0afb2381291dd7ea1def3d6f10e55ce5
SergeyFedorov1991 <serstalk@gmail.com>
2020-07-10 16:02:05 +0300

    add class ChatHistory

--- src/main/java/SocketChat/Config.java
+++ src/main/java/SocketChat/Config.java
@@ -10,15 +10,18 @@ public class Config {
 
     public static int PORT;
     public static String HOST;
+    public static String HELLO_MESSAGE;
+    public static int HISTORY_LENGTH;
 
     static {
         Properties properties = new Properties();
 
         try(FileInputStream propertiesFile = new FileInputStream(PROPERTIES_FILE)) {
             properties.load(propertiesFile);
-            PORT = Integer.parseInt(properties.getProperty("PORT"));
-            HOST = properties.getProperty("HOST");
-
+            PORT           = Integer.parseInt(properties.getProperty("PORT"));
+            HOST           = properties.getProperty("HOST");
+            HELLO_MESSAGE  = properties.getProperty("HELLO_MESSAGE");
+            HISTORY_LENGTH = Integer.parseInt(properties.getProperty("HISTORY_LENGTH"));
 
         } catch (FileNotFoundException e) {
             System.err.println("Properties config file not found");
